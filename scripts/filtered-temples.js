037f64d3cfe34ec37c9e28ad8d6360e8a6df3411
liliankennedy23-png <liliankennedy23@gmail.com>
2026-01-26 20:42:58 -0500

    Update filtered-temples.js

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -4,28 +4,63 @@ const temples = [
     location: "Salt Lake City, Utah",
     dedicated: 1893,
     area: 253015,
-    image: "images/Salt-Lake-Temple.webp"
+    image: "images/salt-lake.webp"
   },
   {
     name: "Paris France Temple",
     location: "Paris, France",
     dedicated: 2017,
     area: 44175,
-    image: "images/Paris-France-Temple.webp"
+    image: "images/paris.webp"
   },
   {
     name: "Seattle Washington Temple",
     location: "Seattle, Washington",
     dedicated: 1980,
     area: 110000,
-    image: "images/Seattle-Washington-Temple.webp"
+    image: "images/seattle.webp"
   },
   {
     name: "Rome Italy Temple",
     location: "Rome, Italy",
     dedicated: 2019,
     area: 40000,
-    image: "images/Rome-Italy-Temple.webp"
+    image: "images/rome.webp"
+  },
+  {
+    name: "Laie Hawaii Temple",
+    location: "Laie, Hawaii",
+    dedicated: 1919,
+    area: 42100,
+    image: "images/Laie-Hawaii-Temple.webp"
+  },
+  {
+    name: "Tokyo Japan Temple",
+    location: "Tokyo, Japan",
+    dedicated: 1980,
+    area: 52700,
+    image: "images/Tokyo-Japan-Temple.webp"
+  },
+  {
+    name: "London England Temple",
+    location: "London, England",
+    dedicated: 1958,
+    area: 42652,
+    image: "images/London-England-Temple.webp"
+  },
+  {
+    name: "San Diego California Temple",
+    location: "San Diego, California",
+    dedicated: 1993,
+    area: 72000,
+    image: "images/San-Diego-Temple.webp"
+  },
+  {
+    name: "Columbia South Carolina Temple",
+    location: "Columbia, South Carolina",
+    dedicated: 1999,
+    area: 10700,
+    image: "images/South-Carolina-Temple.webp"
   }
 ];
 
@@ -78,3 +113,4 @@ document.getElementById("lastModified").textContent = document.lastModified;
 
 /* INITIAL LOAD */
 displayTemples(temples);
+
